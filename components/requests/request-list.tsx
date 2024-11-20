dc98182c5696f15c4816ae439e1f262f2f3b09bc
Faizol Shobola <shobolafaizol@gmail.com>
2024-11-20 14:03:53 +0100

    feat: enhance request list filtering based on user roles and implement real-time updates

--- components/requests/request-list.tsx
+++ components/requests/request-list.tsx
@@ -13,22 +13,30 @@ import { Badge } from "@/components/ui/badge";
 import { format } from "date-fns";
 import type { PettyCashRequest } from "@/types";
 import { useAuth } from "../context/authContext";
-import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { collection, getDocs, onSnapshot, orderBy, query, where } from "firebase/firestore";
 import { db } from "@/lib/firebase";
 
 
 export function RequestList() {
-  const { user } = useAuth();
+  const { user, role } = useAuth()!;
 
   const [filteredRequests, setFilteredRequests] = useState<PettyCashRequest[]>([]);
 
   useEffect(() => {
     const fetchRequests = async () => {
       const requestCollection = collection(db, "requests");
-      const q = query(requestCollection, where("requesterId", "==", user.uid));
-      const requestSnapshot = await getDocs(q);
-      setFilteredRequests(requestSnapshot.docs.map((doc) => doc.data() as PettyCashRequest));
-      console.log(requestSnapshot.docs.map((doc) => doc.data() as PettyCashRequest));
+      let q;
+      if (role === "administrator" || role === "superior") {
+        q = query(requestCollection);
+      } else if(role === "accountant") {
+        q = query(requestCollection, where("status", "==", "approved"));
+      } else {
+        q = query(requestCollection, where("requesterId", "==", user?.uid));
+      }
+      const requestSnapshot = await onSnapshot(q, (snapshot) => {
+        const requests: Request[] = snapshot.docs.map((doc) => doc.data() as PettyCashRequest)
+        setFilteredRequests(requests);
+      });
     };
 
     fetchRequests();
@@ -40,19 +48,23 @@ export function RequestList() {
         <TableHeader>
           <TableRow>
             <TableHead>Date</TableHead>
-            <TableHead>Requester</TableHead>
+            {(role === "administrator" || role === "superior" || role === "accountant") &&
+              <TableHead>Requester</TableHead>
+            }
             <TableHead>Purpose</TableHead>
             <TableHead>Amount</TableHead>
             <TableHead>Status</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          {filteredRequests.map((request) => (
-            <TableRow key={request.id}>
+          {filteredRequests.map((request, index) => (
+            <TableRow key={request.id || index}>
               <TableCell>
-                {format(new Date(request.createdAt.seconds * 1000), "MMM d, yyyy")}
+                {request.createdAt && format(new Date(request.createdAt.seconds * 1000), "MMM d, yyyy")}
               </TableCell>
-              <TableCell>{request.requesterName}</TableCell>
+                {(role === "administrator" || role === "superior" || role === "accountant") && 
+                  <TableCell>{request.requesterName}</TableCell>
+                }
               <TableCell>{request.purpose}</TableCell>
               <TableCell>${request.amount.toFixed(2)}</TableCell>
               <TableCell>
