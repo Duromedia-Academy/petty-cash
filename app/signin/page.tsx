e6f5b0ee43dc0cabc95f617e23a91b922d12f7a7
shobola faruq <shobolafaruq@gmail.com>
2025-01-10 16:58:00 +0000

    feat: update auth page component layout

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -7,7 +7,7 @@ export default function Register() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-zinc-900 to-zinc-950">
-      <div className="container relative flex flex-col items-center justify-center lg:px-0">
+      <div className="container relative flex flex-col items-center justify-center px-6">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight text-white">
